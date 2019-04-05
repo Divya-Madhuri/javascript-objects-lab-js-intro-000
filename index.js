4bcaea4f39a39d6c9f00c72fbc571d076b427bdc
Divya <alaganduladivyamadhuri@gmail.com>
2019-04-05 13:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
+  const newObj = Object.assign({}, object)
   delete object[key]
 }
 
